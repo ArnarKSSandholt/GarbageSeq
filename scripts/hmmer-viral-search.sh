df6bb8685c88abfc3600e14c6ed28631512e002a
ljmesi <37740329+ljmesi@users.noreply.github.com>
2019-11-24 08:00:19 +0000

    Remove unnecessary parts Remove one loop with only one iteration. Remove also variable assignments that aren't used further on. Adjust also so that each fOTU each studied at a time rather than each hmm at a time. This will make it easier to see if the run is going correctly.

--- scripts/hmmer-viral-search.sh
+++ scripts/hmmer-viral-search.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -A snic2019-3-22
 #SBATCH -t 70:00:00
-#SBATCH -p core -n 10
+#SBATCH -p core -n 15
 #SBATCH -o hmmer-%j.out
 #SBATCH -J hmmer-viral-search
 #SBATCH --mail-type=ALL
@@ -11,26 +11,20 @@
 module load bioinfo-tools
 module load hmmer/3.2.1
 
-DATA="../analyses/HMM_scan_using_eggNOG_HMMs/eggNOG_data/HMMs/"
+DATA="../analyses/HMM_scan_using_eggNOG_HMMs/eggNOG_data/HMMs/10239/"
 fOTUproteoms_DIR="../analyses/fOTU_proteomes/"
 HMMer_OUTPUT_DIR="../analyses/HMM_scan_using_eggNOG_HMMs/hmmsearch_out_viruses/"
-
+TAXID_NO="10239"
 
 read -r -a fOTUproteoms <<< $( find $fOTUproteoms_DIR -name "*.faa" -and -type f -print0 | xargs -0 echo )
-# Take time of this run
-#SECONDS=0
-for TAXID in $DATA*/ ; do
-  TAXID_NO=$(echo "$TAXID" | awk -F "/" '{print $(NF-1)}')
-  read -r -a HMMs <<< $( find $TAXID -name "*.hmm" -and -type f -print0 | xargs -0 echo )
+read -r -a HMMs <<< $( find $DATA -name "*.hmm" -and -type f -print0 | xargs -0 echo )
+
+for fOTUproteom in "${fOTUproteoms[@]}"; do
+  #fOTU_FILE=$(echo $(basename "$fOTUproteom"))
+  fOTU=$(echo $(basename "$fOTUproteom") | awk -F "." '{print $1}')
   for HMM in "${HMMs[@]}"; do
-    HMM_FILE=$(echo $(basename "$HMM"))
+    #HMM_FILE=$(echo $(basename "$HMM"))
     HMM_VARIANT=$(echo $(basename "$HMM") | awk -F "." '{print $1}')
-    for fOTUproteom in "${fOTUproteoms[@]}"; do
-      fOTU_FILE=$(echo $(basename "$fOTUproteom"))
-      fOTU=$(echo $(basename "$fOTUproteom") | awk -F "." '{print $1}')
-      hmmsearch --cpu 10 --noali --notextw -E 0.1 --domE 0.1 --incE 0.01 --incdomE 0.01 "$HMM" "$fOTUproteom" >> "$HMMer_OUTPUT_DIR""$TAXID_NO""-with-""$fOTU"".out"
-    done
+    hmmsearch --cpu 15 --noali --notextw -E 0.1 --domE 0.1 --incE 0.01 --incdomE 0.01 "$HMM" "$fOTUproteom" >> "$HMMer_OUTPUT_DIR""$TAXID_NO""-with-""$fOTU"".out"
   done
 done
-#duration=$SECONDS
-#echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
