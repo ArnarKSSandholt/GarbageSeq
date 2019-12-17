8dd0624eccaeaa3f5574f61e0a54e2594b5139cb
ljmesi <37740329+ljmesi@users.noreply.github.com>
2019-12-17 12:37:58 +0100

    Update sbatch core No and paths

--- scripts/hmmer-viral-search.sh
+++ scripts/hmmer-viral-search.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
 #SBATCH -A snic2019-3-22
-#SBATCH -t 70:00:00
-#SBATCH -p core -n 15
+#SBATCH -t 96:00:00
+#SBATCH -p core -n 5
 #SBATCH -o hmmer-%j.out
 #SBATCH -J hmmer-viral-search
 #SBATCH --mail-type=ALL
@@ -11,9 +11,9 @@
 module load bioinfo-tools
 module load hmmer/3.2.1
 
-DATA="../analyses/HMM_scan_using_eggNOG_HMMs/eggNOG_data/HMMs/10239/"
-fOTUproteoms_DIR="../analyses/fOTU_proteomes/"
-HMMer_OUTPUT_DIR="../analyses/HMM_scan_using_eggNOG_HMMs/hmmsearch_out_viruses/"
+DATA="../analyses/HMMsearch/eggNOG_data/HMMs/10239/"
+fOTUproteoms_DIR="../analyses/fOTU_proteomes1/"
+HMMer_OUTPUT_DIR="../analyses/HMMsearch/hmmsearch_out_viruses/"
 TAXID_NO="10239"
 
 read -r -a fOTUproteoms <<< $( find $fOTUproteoms_DIR -name "*.faa" -and -type f -print0 | xargs -0 echo )
@@ -25,6 +25,6 @@ for fOTUproteom in "${fOTUproteoms[@]}"; do
   for HMM in "${HMMs[@]}"; do
     #HMM_FILE=$(echo $(basename "$HMM"))
     HMM_VARIANT=$(echo $(basename "$HMM") | awk -F "." '{print $1}')
-    hmmsearch --cpu 15 --noali --notextw -E 0.1 --domE 0.1 --incE 0.01 --incdomE 0.01 "$HMM" "$fOTUproteom" >> "$HMMer_OUTPUT_DIR""$TAXID_NO""-with-""$fOTU"".out"
+    hmmsearch --cpu 5 --noali --notextw -E 0.1 --domE 0.1 --incE 0.01 --incdomE 0.01 "$HMM" "$fOTUproteom" >> "$HMMer_OUTPUT_DIR""$TAXID_NO""-with-""$fOTU"".out"
   done
 done
