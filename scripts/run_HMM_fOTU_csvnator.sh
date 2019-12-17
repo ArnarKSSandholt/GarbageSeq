fbbcacd15fd933b12f011504b2db53fc9e7524dc
ljmesi <37740329+ljmesi@users.noreply.github.com>
2019-12-17 17:09:43 +0100

    Define R libraries location globally

--- scripts/run_HMM_fOTU_csvnator.sh
+++ scripts/run_HMM_fOTU_csvnator.sh
@@ -12,6 +12,11 @@ module load bioinfo-tools
 module load R/3.6.1
 
 DATA="../analyses/HMMsearch/hmmsearch_out_parsed/"
+# Define where my R libraries are located
+R_LIBS_USER="/home/lame5423/R/x86_64-redhat-linux-gnu-library/3.6/"
+
+export R_LIBS_USER
+
 HMM_fOTU_OUTPUT_DIR="../analyses/HMMsearch/"
 
 Rscript HMM_fOTU_csvnator.R     \
