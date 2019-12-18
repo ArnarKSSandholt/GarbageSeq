b092b45025b96e7944f8345ab5f9372501f3ff21
ljmesi <37740329+ljmesi@users.noreply.github.com>
2019-12-18 13:33:18 +0000

    Comment code and handle assigning the mode

--- scripts/run_HMM_fOTU_csvnator.sh
+++ scripts/run_HMM_fOTU_csvnator.sh
@@ -11,14 +11,18 @@
 module load bioinfo-tools
 module load R/3.6.1
 
+# Where is the parsed input data located?
 DATA="../analyses/HMMsearch/hmmsearch_out_parsed/"
 # Define where my R libraries are located
 R_LIBS_USER="/home/lame5423/R/x86_64-redhat-linux-gnu-library/3.6/"
-
+# Are we taking the average or the maximum -log e-value?
+MODE="max"
+# The libraries location must be exported so it's visible for the R script
 export R_LIBS_USER
 
 HMM_fOTU_OUTPUT_DIR="../analyses/HMMsearch/"
 
 Rscript HMM_fOTU_csvnator.R     \
         --pathToParsed="$DATA" \
-        --pathToOutput="$HMM_fOTU_OUTPUT_DIR" 
+        --pathToOutput="$HMM_fOTU_OUTPUT_DIR" \
+        --maxOrAvg="$MODE"
