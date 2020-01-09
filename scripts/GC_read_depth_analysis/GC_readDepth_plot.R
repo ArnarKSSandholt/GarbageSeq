390ba3a6a47111f1816c539e8eaa07cf8264fd4d
ArnarKSSandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-01-09 19:35:56 +0000

    Updated some plots and the corresponding code

--- scripts/GC_read_depth_analysis/GC_readDepth_plot.R
+++ scripts/GC_read_depth_analysis/GC_readDepth_plot.R
@@ -10,29 +10,31 @@ df1 <- read.csv(data_file_1)
 df2 <- read.csv(data_file_2)
 df3 <- read.csv(data_file_3)
 
-p1 <- ggplot(df1[df1$length>10000,], aes(x=GC, y=total_avg_depth, size=length, label = name))+geom_point()+scale_y_log10()+scale_x_log10()+theme_minimal()
-p1
-
 plot_path <- paste(c("/home/arnar/Documents/team_garbageSeq/data/plots/GC_read_depth_plots/", "read_depth_vs_GC_plain.png"), collapse = "")
-png(filename = plot_path, width = 1280, height = 720)
+png(filename = plot_path, width = 1075, height = 589)
 ggplot(df2[df2$length>10000,], aes(x=GC, y=total_avg_depth, size=length, label = fOTU_name)) + 
   geom_point() + scale_y_log10() + scale_x_log10() + theme_minimal() + ggtitle("Total average read depth vs GC percentage \n of fOTUs") + 
   theme(plot.title = element_text(hjust = 0.5, size = 24), axis.title.x = element_text(size = 16), axis.title.y = element_text(size = 16)) + 
-  labs(x = "GC percentage", y = "Total average read depth")
+  labs(x = "GC percentage", y = "Total average read depth") + 
+  guides(size = guide_legend(title="fOTU size"))
 dev.off()
 
 plot_path <- paste(c("/home/arnar/Documents/team_garbageSeq/data/plots/GC_read_depth_plots/", "read_depth_vs_GC_VirFinder.png"), collapse = "")
-png(filename = plot_path, width = 1280, height = 720)
+png(filename = plot_path, width = 1075, height = 589)
 ggplot(df3[df3$length>10000,], aes(x=GC, y=total_avg_depth, size=length, label = fOTU_name, col = viral_content)) + 
   geom_point() + scale_y_log10() + scale_x_log10() + theme_minimal() + ggtitle("Total average read depth vs GC percentage \n of fOTUs with VirFinder") + 
   theme(plot.title = element_text(hjust = 0.5, size = 24), axis.title.x = element_text(size = 16), axis.title.y = element_text(size = 16)) + 
-  labs(x = "GC percentage", y = "Total average read depth")
+  labs(x = "GC percentage", y = "Total average read depth") + 
+  guides(size = guide_legend(title="fOTU size"), colour = guide_legend(title="Viral content \nfraction")) + 
+  scale_color_hue(labels = c("High", "Low", "Medium", "Very high"))
 dev.off()
 
 plot_path <- paste(c("/home/arnar/Documents/team_garbageSeq/data/plots/GC_read_depth_plots/", "read_depth_vs_GC_PlasFlow.png"), collapse = "")
-png(filename = plot_path, width = 1280, height = 720)
+png(filename = plot_path, width = 1075, height = 589)
 ggplot(df3[df3$length>10000,], aes(x=GC, y=total_avg_depth, size=length, label = fOTU_name, col = fOTU_composition)) + 
   geom_point() + scale_y_log10() + scale_x_log10() + theme_minimal() + ggtitle("Total average read depth vs GC percentage \n of fOTUs with PlasFlow") + 
   theme(plot.title = element_text(hjust = 0.5, size = 24), axis.title.x = element_text(size = 16), axis.title.y = element_text(size = 16)) + 
-  labs(x = "GC percentage", y = "Total average read depth")
+  labs(x = "GC percentage", y = "Total average read depth") + 
+  guides(size = guide_legend(title="fOTU size"), colour = guide_legend(title="Plasflow content \ncategories")) + 
+  scale_color_hue(labels = c("Bacterial \nchromosome", "Mixed", "Plasmid", "Unclassified"))
 dev.off()
